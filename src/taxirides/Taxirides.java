b7d4a53fa8611609885f6ccf1e894d929044e442
Tiko Huizinga <thuizinga@cs.ru.nl>
2016-06-08 21:54:40 +0200

    Project first setup with authors added

---
+++ src/taxirides/Taxirides.java
@@ -0,0 +1,18 @@
+package taxirides;
+
+/**
+ *
+ * @author Tiko Huizinga - s4460898
+ * @author Conny Blach - s4329872
+ * 
+ */
+public class Taxirides {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
